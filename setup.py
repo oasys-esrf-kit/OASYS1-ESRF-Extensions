20a4db85a14b39c44ae493e3b6b13e5f8178d6af
Manuel Sanchez Del Rio <srio@esrf.eu>
2021-02-23 16:58:34 +0100

    diagonalization ok

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ except AttributeError:
     from setuptools import find_packages, setup
 
 NAME = 'OASYS1-ESRF-Extensions'
-VERSION = '0.0.27'
+VERSION = '0.0.28'
 ISRELEASED = True
 
 DESCRIPTION = 'OASYS extension for the ESRF'
