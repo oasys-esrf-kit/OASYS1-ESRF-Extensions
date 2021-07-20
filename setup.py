c1646e95add2243c5222b4965f1c3db9321d1aca
Manuel Sanchez del Rio <srio@esrf.eu>
2021-07-20 11:24:02 +0200

    intensity info added

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ except AttributeError:
     from setuptools import find_packages, setup
 
 NAME = 'OASYS1-ESRF-Extensions'
-VERSION = '0.0.36'
+VERSION = '0.0.37'
 ISRELEASED = True
 
 DESCRIPTION = 'OASYS extension for the ESRF'
