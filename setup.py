8a525e2a81f89d9481014bc0b720ef1ca6e4bcb9
Manuel Sanchez del Rio <srio@esrf.eu>
2022-05-23 11:59:10 +0200

    update

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ except AttributeError:
     from setuptools import find_packages, setup
 
 NAME = 'OASYS1-ESRF-Extensions'
-VERSION = '0.0.57'
+VERSION = '0.0.58'
 ISRELEASED = True
 
 DESCRIPTION = 'OASYS extension for the ESRF'
