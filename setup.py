2dcdb3bb565a35bdc6ae0d4316ac2a96b801e4e3
Manuel Sanchez del Rio <srio@esrf.eu>
2023-03-21 14:23:06 +0100

    wolter utilities

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ except AttributeError:
     from setuptools import find_packages, setup
 
 NAME = 'OASYS1-ESRF-Extensions'
-VERSION = '0.0.64'
+VERSION = '0.0.65'
 ISRELEASED = True
 
 DESCRIPTION = 'OASYS extension for the ESRF'
