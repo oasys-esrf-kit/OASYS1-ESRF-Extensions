73400c788ddd3cd5d8a85dc3044c6c25b03b7e52
Juan Reyes <jureyherrera@gmail.com>
2024-04-16 17:12:30 +0200

    Adding new bm05 attenuators box

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ except AttributeError:
     from setuptools import find_packages, setup
 
 NAME = 'OASYS1-ESRF-Extensions'
-VERSION = '0.0.77'
+VERSION = '0.0.78'
 ISRELEASED = True
 
 DESCRIPTION = 'OASYS extension for the ESRF'
