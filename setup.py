e85790318cbb2932968a83109e69bd2e585f7eba
Manuel Sanchez del Rio <srio@esrf.eu>
2024-11-13 10:14:25 +0100

    cleaning  wofry apps already moved to main wofry

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ except AttributeError:
     from setuptools import find_packages, setup
 
 NAME = 'OASYS1-ESRF-Extensions'
-VERSION = '0.0.82'
+VERSION = '0.0.83'
 ISRELEASED = True
 
 DESCRIPTION = 'OASYS extension for the ESRF'
